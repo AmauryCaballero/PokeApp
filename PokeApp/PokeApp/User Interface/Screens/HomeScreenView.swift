5eab2ce56a7c20cb5d2f13da098d15f49b8fb06a
Amaury Miguel Caballero <62359438+AmauryCaballero@users.noreply.github.com>
2024-04-22 01:51:05 -0400

    feat: create home screen view

---
+++ PokeApp/PokeApp/User Interface/Screens/HomeScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreenView.swift
+//  PokeApp
+//
+//  Created by Amaury Caballero on 22/4/24.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeScreenView()
+}
