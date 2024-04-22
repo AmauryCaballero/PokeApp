7345c746418c04994f6644d6b8ce0c532beaf829
Amaury Miguel Caballero <62359438+AmauryCaballero@users.noreply.github.com>
2024-04-22 01:02:55 -0400

    feat: network service implementation

--- PokeApp/PokeApp/Model/APIEndpoint.swift
+++ PokeApp/PokeApp/Model/APIEndpoint.swift
@@ -8,15 +8,23 @@
 import Foundation
 
 enum APIEndpoint {
-    case pokemonList
-    case pokemonDetail(String)
-    
+    case pokemonList(limit: Int, offset: Int)
+    case pokemonDetail(Int)
+    case pokemonByName(String)
+
     var url: URL {
         switch self {
-        case .pokemonList:
-            return APIConfig.baseURL.appendingPathComponent("pokemon")
+        case .pokemonList(let limit, let offset):
+            var components = URLComponents(url: APIConfig.baseURL.appendingPathComponent("pokemon"), resolvingAgainstBaseURL: true)!
+            components.queryItems = [
+                URLQueryItem(name: "limit", value: String(limit)),
+                URLQueryItem(name: "offset", value: String(offset))
+            ]
+            return components.url!
         case .pokemonDetail(let id):
             return APIConfig.baseURL.appendingPathComponent("pokemon/\(id)")
+        case .pokemonByName(let name):
+            return APIConfig.baseURL.appendingPathComponent("pokemon/\(name)")
         }
     }
 }
--- PokeApp/PokeApp/Model/PokemonDetail.swift
+++ PokeApp/PokeApp/Model/PokemonDetail.swift
@@ -8,52 +8,66 @@
 import Foundation
 
 struct PokemonDetail: Codable {
-    let abilities: [PokemonAbility]
-    let forms: [NamedAPIResource]
-    let gameIndices: [VersionGameIndex]
-    let heldItems: [PokemonHeldItem]
-    let moves: [PokemonMove]
-    let species: NamedAPIResource
-    let stats: [PokemonStat]
-    let types: [PokemonType]
+    let id: Int?
+    let name: String?
+    let baseExperience: Int?
+    let height: Int?
+    let isDefault: Bool?
+    let order: Int?
+    let weight: Int?
+    let abilities: [PokemonAbility]?
+    let forms: [NamedAPIResource]?
+    let gameIndices: [VersionGameIndex]?
+    let heldItems: [PokemonHeldItem]?
+    let locationAreaEncounters: String?
+    let moves: [PokemonMove]?
+    let species: NamedAPIResource?
+    let sprites: PokemonSprites?
+    let stats: [PokemonStat]?
+    let types: [PokemonType]?
 }
 
 struct PokemonAbility: Codable {
-    let isHidden: Bool
-    let slot: Int
-    let ability: NamedAPIResource
+    let isHidden: Bool?
+    let slot: Int?
+    let ability: NamedAPIResource?
 }
 
 struct VersionGameIndex: Codable {
-    let gameIndex: Int
-    let version: NamedAPIResource
+    let gameIndex: Int?
+    let version: NamedAPIResource?
 }
 
 struct PokemonHeldItem: Codable {
-    let item: NamedAPIResource
-    let versionDetails: [PokemonHeldItemVersion]
+    let item: NamedAPIResource?
+    let versionDetails: [PokemonHeldItemVersion]?
 }
 
 struct PokemonHeldItemVersion: Codable {
-    let version: NamedAPIResource
-    let rarity: Int
+    let version: NamedAPIResource?
+    let rarity: Int?
 }
 
 struct PokemonMove: Codable {
-    let move: NamedAPIResource
-    let versionGroupDetails: [PokemonMoveVersion]
+    let move: NamedAPIResource?
+    let versionGroupDetails: [PokemonMoveVersion]?
 }
 
 struct PokemonMoveVersion: Codable {
-    let moveLearnMethod: NamedAPIResource
-    let versionGroup: NamedAPIResource
-    let levelLearnedAt: Int
+    let moveLearnMethod: NamedAPIResource?
+    let versionGroup: NamedAPIResource?
+    let levelLearnedAt: Int?
+}
+
+struct PokemonSprites: Codable {
+    let frontDefault: URL?
+    let frontShiny: URL?
 }
 
 struct PokemonStat: Codable {
-    let stat: NamedAPIResource
-    let effort: Int
-    let baseStat: Int
+    let stat: NamedAPIResource?
+    let effort: Int?
+    let baseStat: Int?
 }
 
 struct PokemonType: Codable {
