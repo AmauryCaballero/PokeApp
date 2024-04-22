a1e507eb15f199b19fa80702ee07238a72b9cb62
Amaury Miguel Caballero <62359438+AmauryCaballero@users.noreply.github.com>
2024-04-22 01:15:05 -0400

    Feat/network management documentation (#3)
    * doc: network service protocol comments
    
    * doc!: network module readme

--- PokeApp/PokeApp/Services/Protocols/NetworkServiceProtocol.swift
+++ PokeApp/PokeApp/Services/Protocols/NetworkServiceProtocol.swift
@@ -7,10 +7,25 @@
 
 import Combine
 
+/// Protocol `NetworkServiceProtocol` defines the networking capabilities for fetching Pokemon data from a remote server.
 protocol NetworkServiceProtocol {
+    /// Fetches a list of Pokemon with pagination support.
+    /// - Parameters:
+    ///   - limit: The maximum number of Pokemon entries to return in one call.
+    ///   - offset: The number of Pokemon entries to skip for pagination.
+    /// - Returns: An `AnyPublisher` that outputs `PokemonListResponse` and errors out with `Error`.
+    /// This method uses the Pokemon API to retrieve a list of Pokemon entities specifying how many entries to fetch and how many to skip.
     func fetchPokemonList(limit: Int, offset: Int) -> AnyPublisher<PokemonListResponse, Error>
     
+    /// Fetches detailed information about a specific Pokemon by its unique identifier.
+    /// - Parameter id: The unique identifier of the Pokemon.
+    /// - Returns: An `AnyPublisher` that outputs `PokemonDetail` and errors out with `Error`.
+    /// This method retrieves all relevant details for a Pokemon, identified by its `id`, from the Pokemon API.
     func fetchPokemonDetails(id: Int) -> AnyPublisher<PokemonDetail, Error>
     
+    /// Searches for Pokemon by their name.
+    /// - Parameter name: The name of the Pokemon to search for.
+    /// - Returns: An `AnyPublisher` that outputs `PokemonListResponse` and errors out with `Error`.
+    /// This method searches the Pokemon API for a Pokemon by its name and returns the result in a list format, which might include similar named Pokemon.
     func searchPokemonByName(name: String) -> AnyPublisher<PokemonListResponse, Error>
 }
