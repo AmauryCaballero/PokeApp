1cbd0c15f22cb57594edd1e88dcaadd8cf2fabe5
Amaury Miguel Caballero <62359438+AmauryCaballero@users.noreply.github.com>
2024-04-23 00:18:13 -0400

    feat: create home screen view model

---
+++ PokeApp/PokeApp/Business Logic/ViewModel/HomeScreenViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeScreenViewModel.swift
+//  PokeApp
+//
+//  Created by Amaury Caballero on 22/4/24.
+//
+
+import Foundation
+
+class HomeScreenViewModel: BaseViewModel {
+    override init(networkService: any NetworkServiceProtocol) {
+        super.init(networkService: networkService)
+    }
+}
