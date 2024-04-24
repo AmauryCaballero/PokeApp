53148308305a1055794dffb272557a5736152ffd
Amaury Miguel Caballero <62359438+AmauryCaballero@users.noreply.github.com>
2024-04-24 03:50:37 -0400

    feat: creating `NavigationParametersKeys`

---
+++ PokeApp/PokeApp/Navigation/NavigationParametersKeys.swift
@@ -0,0 +1,12 @@
+//
+//  NavigationParametersKeys.swift
+//  PokeApp
+//
+//  Created by Amaury Caballero on 24/4/24.
+//
+
+import Foundation
+
+enum NavigationParametersKeys: String {
+    case pokemonDetail
+}
